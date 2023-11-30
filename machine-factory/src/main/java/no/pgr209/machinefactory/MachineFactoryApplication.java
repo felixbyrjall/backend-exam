4b74fd518a53ebcc55c6c48d8f8453c4b68b2c4d
NXTDTA <Henry@nextdata.no>
2023-11-30 17:45:06 +0100

    initial commit

---
+++ machine-factory/src/main/java/no/pgr209/machinefactory/MachineFactoryApplication.java
@@ -0,0 +1,13 @@
+package no.pgr209.machinefactory;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MachineFactoryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MachineFactoryApplication.class, args);
+	}
+
+}
---
+++ machine-factory/src/test/java/no/pgr209/machinefactory/MachineFactoryApplicationTests.java
@@ -0,0 +1,13 @@
+package no.pgr209.machinefactory;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MachineFactoryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
