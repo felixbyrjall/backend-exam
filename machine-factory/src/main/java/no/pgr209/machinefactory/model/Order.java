b9136a6cd0213f3b135529e6ee5e307fb9d157c4
NXTDTA <Henry@nextdata.no>
2023-12-01 21:41:14 +0100

    Order Model created

--- machine-factory/src/main/java/no/pgr209/machinefactory/model/Order.java
+++ machine-factory/src/main/java/no/pgr209/machinefactory/model/Order.java
@@ -1,4 +1,41 @@
 package no.pgr209.machinefactory.model;
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@Entity
 public class Order {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "order_seq_gen")
+    @SequenceGenerator(name = "order_seq_gen", sequenceName = "order_seq", allocationSize = 1)
+    @Column(name = "order_id")
+    private Long orderId = 0L;
+
+    @Column(name = "order_date")
+    private LocalDateTime orderDate;
+
+    @ManyToMany(cascade = CascadeType.ALL)
+    @JoinTable(
+            name = "order_machine",
+            joinColumns = @JoinColumn(name = "order_id"),
+            inverseJoinColumns = @JoinColumn(name = "machine_id")
+    )
+    @JsonIgnoreProperties("machine")
+    private List<Machine> machines = new ArrayList<>();
+
+    // Constructors, getters, setters, and methods below.
+    public void Machine(LocalDateTime orderDate) {
+        this.orderDate = orderDate;
+    }
 }
