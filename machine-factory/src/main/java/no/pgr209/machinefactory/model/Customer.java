2ba30428d09b4058bc22ccc746ee20c55df96fc0
NXTDTA <Henry@nextdata.no>
2023-12-01 21:02:45 +0100

    created Customer model with a joined table together with Address

--- machine-factory/src/main/java/no/pgr209/machinefactory/model/Customer.java
+++ machine-factory/src/main/java/no/pgr209/machinefactory/model/Customer.java
@@ -1,4 +1,45 @@
 package no.pgr209.machinefactory.model;
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@Entity
 public class Customer {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "customer_seq_gen")
+    @SequenceGenerator(name = "customer_seq_gen", sequenceName = "customer_seq", allocationSize = 1)
+    @Column(name = "customer_id")
+    private Long customerId = 0L;
+
+    @Column(name = "customer_name")
+    private String customerName;
+
+    @Column(name = "customer_email")
+    private String customerEmail;
+
+    @ManyToMany(cascade = CascadeType.ALL)
+    @JoinTable(
+            name = "customer_address",
+            joinColumns = @JoinColumn(name = "customer_id"),
+            inverseJoinColumns = @JoinColumn(name = "address_id")
+    )
+    @JsonIgnoreProperties("customers")
+    private List<Address> addresses = new ArrayList<>();
+
+    // Constructors, getters, setters, and methods below.
+    public Customer(String customerName, String customerEmail) {
+        this.customerName = customerName;
+        this.customerEmail = customerEmail;
+    }
+
 }
