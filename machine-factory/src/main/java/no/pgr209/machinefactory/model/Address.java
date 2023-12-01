475fed1ab13c98bcd507db0b4eb9442cfcb15bb0
NXTDTA <Henry@nextdata.no>
2023-12-01 21:24:56 +0100

    created Address model, and made JoinTable bidirectional, coming from both models

--- machine-factory/src/main/java/no/pgr209/machinefactory/model/Address.java
+++ machine-factory/src/main/java/no/pgr209/machinefactory/model/Address.java
@@ -1,4 +1,48 @@
 package no.pgr209.machinefactory.model;
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@Entity
 public class Address {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "address_seq_gen")
+    @SequenceGenerator(name = "address_seq_gen", sequenceName = "address_seq", allocationSize = 1)
+    @Column(name = "address_id")
+    private Long addressId = 0L;
+
+    @Column(name = "address_street")
+    private String addressStreet;
+
+    @Column(name = "address_city")
+    private String addressCity;
+
+    @Column(name = "address_zip")
+    private Integer addressZip;
+
+    @ManyToMany(cascade = CascadeType.ALL)
+    @JoinTable(
+            name = "customer_address",
+            joinColumns = @JoinColumn(name = "address_id"),
+            inverseJoinColumns = @JoinColumn(name = "customer_id")
+    )
+    @JsonIgnoreProperties("customers")
+    private List<Customer> customers = new ArrayList<>();
+
+    // Constructors, getters, setters, and methods below.
+    public Address(String addressStreet, String addressCity, Integer addressZip) {
+        this.addressStreet = addressStreet;
+        this.addressCity = addressCity;
+        this.addressZip = addressZip;
+    }
 }
