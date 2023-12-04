11fdc2ed82f9aea6f9c0427d1a2da8ae09c18287
Felix Byrjall <felixbyrjall@gmail.com>
2023-12-04 17:59:06 +0100

    completed AddressService

--- machine-factory/src/main/java/no/pgr209/machinefactory/service/AddressService.java
+++ machine-factory/src/main/java/no/pgr209/machinefactory/service/AddressService.java
@@ -1,4 +1,34 @@
 package no.pgr209.machinefactory.service;
 
+import no.pgr209.machinefactory.model.Address;
+import no.pgr209.machinefactory.repo.AddressRepo;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
 public class AddressService {
+    private final AddressRepo addressRepo;
+
+    @Autowired
+    public AddressService(AddressRepo addressRepo) {
+        this.addressRepo = addressRepo;
+    }
+
+    public List<Address> getAllAddresses() {
+        return addressRepo.findAll();
+    }
+
+    public Address getAddressById(Long id) {
+        return addressRepo.findById(id).orElse(null);
+    }
+
+    public Address createAddress(Address address) {
+        return addressRepo.save(address);
+    }
+
+    public void deleteAddress(Long id) {
+        addressRepo.deleteById(id);
+    }
 }
