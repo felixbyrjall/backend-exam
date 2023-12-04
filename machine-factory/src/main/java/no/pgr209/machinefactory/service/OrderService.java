89e5b88e9af43d4210a8d6dd58c788bad2f1dd98
Felix Byrjall <felixbyrjall@gmail.com>
2023-12-04 17:45:39 +0100

    Created getAllOrders. @autowire for the constructor.

--- machine-factory/src/main/java/no/pgr209/machinefactory/service/OrderService.java
+++ machine-factory/src/main/java/no/pgr209/machinefactory/service/OrderService.java
@@ -1,14 +1,26 @@
 package no.pgr209.machinefactory.service;
 
+import no.pgr209.machinefactory.model.Order;
 import no.pgr209.machinefactory.repo.OrderRepo;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 public class OrderService {
     //Connection to the order repository
     private final OrderRepo orderRepo;
 
+    //Constructor for orderRepo
+    @Autowired
     public OrderService(OrderRepo orderRepo) {
         this.orderRepo = orderRepo;
     }
+
+    //Get ALL orders
+    public List<Order> getAllOrders(){
+        return orderRepo.findAll();
+    }
+
 }
