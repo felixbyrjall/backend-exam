8e2d6c1df31f75c904aecf2ff0bf457ac0d79256
Felix Byrjall <felixbyrjall@gmail.com>
2023-12-04 18:10:18 +0100

    completed PartService

--- machine-factory/src/main/java/no/pgr209/machinefactory/service/PartService.java
+++ machine-factory/src/main/java/no/pgr209/machinefactory/service/PartService.java
@@ -1,4 +1,32 @@
 package no.pgr209.machinefactory.service;
 
+import no.pgr209.machinefactory.model.Part;
+import no.pgr209.machinefactory.repo.PartRepo;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
 public class PartService {
+    private final PartRepo partRepo;
+
+    public PartService(PartRepo partRepo) {
+        this.partRepo = partRepo;
+    }
+
+    public List<Part> getAllParts() {
+        return partRepo.findAll();
+    }
+
+    public Part getPartById(Long id) {
+        return partRepo.findById(id).orElse(null);
+    }
+
+    public Part createPart(Part part) {
+        return partRepo.save(part);
+    }
+
+    public void deletePart(Long id) {
+        partRepo.deleteById(id);
+    }
 }
