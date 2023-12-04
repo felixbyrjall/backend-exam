b902576a7dae325e79a5be2ebc79383b0be78986
Felix Byrjall <felixbyrjall@gmail.com>
2023-12-04 18:02:14 +0100

    completed CustomerService

--- machine-factory/src/main/java/no/pgr209/machinefactory/service/CustomerService.java
+++ machine-factory/src/main/java/no/pgr209/machinefactory/service/CustomerService.java
@@ -1,4 +1,34 @@
 package no.pgr209.machinefactory.service;
 
+import no.pgr209.machinefactory.model.Customer;
+import no.pgr209.machinefactory.repo.CustomerRepo;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
 public class CustomerService {
+    private final CustomerRepo customerRepo;
+
+    @Autowired
+    public CustomerService(CustomerRepo customerRepo) {
+        this.customerRepo = customerRepo;
+    }
+
+    public List<Customer> getAllCustomers() {
+        return customerRepo.findAll();
+    }
+
+    public Customer getCustomerById(Long id) {
+        return customerRepo.findById(id).orElse(null);
+    }
+
+    public Customer createCustomer(Customer customer) {
+        return customerRepo.save(customer);
+    }
+
+    public void deleteCustomer(Long id) {
+        customerRepo.deleteById(id);
+    }
 }
