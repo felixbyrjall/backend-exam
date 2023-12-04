5373c1d95a88aabaccadf230326f6d9580a0d79b
Felix Byrjall <felixbyrjall@gmail.com>
2023-12-04 17:38:56 +0100

    Initial commit for the OrderService, created a connection to the order repository and created a constructor

--- machine-factory/src/main/java/no/pgr209/machinefactory/service/OrderService.java
+++ machine-factory/src/main/java/no/pgr209/machinefactory/service/OrderService.java
@@ -1,4 +1,14 @@
 package no.pgr209.machinefactory.service;
 
+import no.pgr209.machinefactory.repo.OrderRepo;
+import org.springframework.stereotype.Service;
+
+@Service
 public class OrderService {
+    //Connection to the order repository
+    private final OrderRepo orderRepo;
+
+    public OrderService(OrderRepo orderRepo) {
+        this.orderRepo = orderRepo;
+    }
 }
