242dae8822e581400b4bcb2f08f5ae63b34b0f2b
Felix Byrjall <felixbyrjall@gmail.com>
2023-12-05 12:45:06 +0100

    completed CustomerController

--- machine-factory/src/main/java/no/pgr209/machinefactory/controller/CustomerController.java
+++ machine-factory/src/main/java/no/pgr209/machinefactory/controller/CustomerController.java
@@ -1,4 +1,40 @@
 package no.pgr209.machinefactory.controller;
 
+import no.pgr209.machinefactory.model.Customer;
+import no.pgr209.machinefactory.service.CustomerService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/customer")
 public class CustomerController {
+
+    private final CustomerService customerService;
+
+    @Autowired
+    public CustomerController(CustomerService customerService) {
+        this.customerService = customerService;
+    }
+
+    @GetMapping
+    public List<Customer> getCustomers() {
+        return customerService.getCustomers();
+    }
+
+    @GetMapping("/{id}")
+    public Customer getCustomerById(@PathVariable Long id) {
+        return customerService.getCustomerById(id);
+    }
+
+    @PostMapping
+    public Customer createCustomer(Customer customer) {
+        return customerService.createCustomer(customer);
+    }
+
+    @DeleteMapping("/{id}")
+    public void deleteCustomer(@PathVariable Long id) {
+        customerService.deleteCustomerById(id);
+    }
 }
