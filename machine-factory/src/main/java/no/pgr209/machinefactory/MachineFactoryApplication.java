e37608b38a263b3576425f19313a4b5aef0326e7
Felix Byrjall <felixbyrjall@gmail.com>
2023-12-05 16:39:03 +0100

    Rebased

--- machine-factory/src/main/java/no/pgr209/machinefactory/MachineFactoryApplication.java
+++ machine-factory/src/main/java/no/pgr209/machinefactory/MachineFactoryApplication.java
@@ -20,7 +20,6 @@ public class MachineFactoryApplication {
 		SpringApplication.run(MachineFactoryApplication.class, args);
 	}
 
-
 	@Bean
 	@Profile("!test") // Seperate CommandLine and Testing.
 	CommandLineRunner commandLineRunner(
@@ -32,27 +31,53 @@ public class MachineFactoryApplication {
 			SubassemblyRepo subassemblyRepo
 	){
 		return args -> {
-			List<Machine> machineList = new ArrayList<>();
+			List<Machine> machineList1 = new ArrayList<>();
 
-			Customer customer = customerRepo.save(new Customer("Ola Nordmann", "ola@nordmann.no"));
-			Address address = addressRepo.save(new Address("Storgata 33", "Oslo", 2204));
+
+			Customer customer1 = customerRepo.save(new Customer("Ola Normann", "ola@normann.no"));
+			Address address1 = addressRepo.save(new Address("Storgata 33", "Oslo", 2204));
 
 			var machine1 = new Machine("3D printer", "Electronics");
 			var machine2 = new Machine("Speaker", "Electronics");
-			machineList.add(machine1);
-			machineList.add(machine2);
+
+			machineList1.add(machine1);
+			machineList1.add(machine2);
 
 			Part part = partRepo.save(new Part("Printer nozzle"));
 			Subassembly subassembly = subassemblyRepo.save(new Subassembly("Printer head"));
 
-			Order order = orderRepo.save(new Order());
+			Order order1 = orderRepo.save(new Order());
 
-			order.setOrderDate(LocalDateTime.now());
-			order.setAddress(address);
-			order.setCustomer(customer);
-			order.setMachines(machineList);
+			order1.setOrderDate(LocalDateTime.now());
+			order1.setAddress(address1);
+			order1.setCustomer(customer1);
+			order1.setMachines(machineList1);
 
-			orderRepo.save(order);
+			orderRepo.save(order1);
+
+			/*List<Machine> machineList2 = new ArrayList<>();
+
+			Customer customer2 = customerRepo.save(new Customer("Kong Harald", "ola@normann.no"));
+
+			Address address2 = addressRepo.save(new Address("Trondheimsveien 21", "Oslo", 1234));
+			Address address3 = addressRepo.save(new Address("Karl Johans gate 12", "Oslo", 4342));
+
+			var machine3 = new Machine("3D printer", "Electronics");
+			var machine4 = new Machine("Speaker", "Electronics");
+			var machine5 = new Machine("Monitor", "Electronics");
+
+			machineList2.add(machine3);
+			machineList2.add(machine4);
+			machineList2.add(machine5);
+
+			Order order2 = orderRepo.save(new Order());
+
+			order2.setOrderDate(LocalDateTime.now());
+			order2.setAddress(address3);
+			order2.setCustomer(customer2);
+			order2.setMachines(machineList2);
+
+			orderRepo.save(order2);*/
 		};
 	}
 }
