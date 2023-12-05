9b0559445e5b2c83c03dff55a00ac4f84b2644fd
Felix Byrjall <felixbyrjall@gmail.com>
2023-12-05 12:06:26 +0100

    completed PartController

--- machine-factory/src/main/java/no/pgr209/machinefactory/controller/PartController.java
+++ machine-factory/src/main/java/no/pgr209/machinefactory/controller/PartController.java
@@ -1,4 +1,40 @@
 package no.pgr209.machinefactory.controller;
 
+import no.pgr209.machinefactory.model.Part;
+import no.pgr209.machinefactory.service.PartService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/parts")
 public class PartController {
+
+    private final PartService partService;
+
+    @Autowired
+    public PartController(PartService partService) {
+        this.partService = partService;
+    }
+
+    @GetMapping
+    public List<Part> getParts() {
+        return partService.getParts();
+    }
+
+    @GetMapping("/{id}")
+    public Part getPartById(@PathVariable Long id) {
+        return partService.getPartById(id);
+    }
+
+    @PostMapping
+    public Part createPart(@RequestBody Part part) {
+        return partService.createPart(part);
+    }
+
+    @DeleteMapping("/{id}")
+    public void deletePart(@PathVariable Long id) {
+        partService.deletePartById(id);
+    }
 }
