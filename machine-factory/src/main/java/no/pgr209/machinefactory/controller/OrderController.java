78474ed91bf6096ec5ef2195a06feaae04eca669
Felix Byrjall <felixbyrjall@gmail.com>
2023-12-05 11:36:19 +0100

    created the start of the OrderController

--- machine-factory/src/main/java/no/pgr209/machinefactory/controller/OrderController.java
+++ machine-factory/src/main/java/no/pgr209/machinefactory/controller/OrderController.java
@@ -1,4 +1,27 @@
 package no.pgr209.machinefactory.controller;
 
+import no.pgr209.machinefactory.model.Order;
+import no.pgr209.machinefactory.service.OrderService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/order")
 public class OrderController {
+
+    private final OrderService orderService;
+
+    @Autowired
+    public OrderController(OrderService orderService) {
+        this.orderService = orderService;
+    }
+
+    @GetMapping
+    public List<Order> getAllOrders(){
+        return orderService.getOrders();
+    }
 }
