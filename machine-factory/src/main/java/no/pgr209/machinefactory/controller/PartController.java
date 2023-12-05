3ebdb0fc6b261890268ccc7271a9577ccf11a475
Felix Byrjall <felixbyrjall@gmail.com>
2023-12-05 12:36:45 +0100

    changed api call from parts to part

--- machine-factory/src/main/java/no/pgr209/machinefactory/controller/PartController.java
+++ machine-factory/src/main/java/no/pgr209/machinefactory/controller/PartController.java
@@ -8,7 +8,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
-@RequestMapping("/api/parts")
+@RequestMapping("/api/part")
 public class PartController {
 
     private final PartService partService;
