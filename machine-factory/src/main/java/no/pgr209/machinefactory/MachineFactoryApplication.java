8db5281e3440435f0f08456ae21493ec2e3e9c64
Felix Byrjall <felixbyrjall@gmail.com>
2023-12-05 15:07:19 +0100

    Added mock info to application

--- machine-factory/src/main/java/no/pgr209/machinefactory/MachineFactoryApplication.java
+++ machine-factory/src/main/java/no/pgr209/machinefactory/MachineFactoryApplication.java
@@ -1,7 +1,15 @@
 package no.pgr209.machinefactory;
 
+import no.pgr209.machinefactory.model.*;
+import no.pgr209.machinefactory.repo.*;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
 
 // Initial
 @SpringBootApplication
@@ -11,4 +19,38 @@ public class MachineFactoryApplication {
 		SpringApplication.run(MachineFactoryApplication.class, args);
 	}
 
+
+	@Bean
+	CommandLineRunner commandLineRunner(
+			AddressRepo addressRepo,
+			CustomerRepo customerRepo,
+			MachineRepo machineRepo,
+			OrderRepo orderRepo,
+			PartRepo partRepo,
+			SubassemblyRepo subassemblyRepo
+	){
+		return args -> {
+			List<Machine> machineList = new ArrayList<>();
+
+			Customer customer = customerRepo.save(new Customer("Ola Normann", "ola@normann.no"));
+			Address address = addressRepo.save(new Address("Storgata 33", "Oslo", 2204));
+
+			var machine1 = new Machine("3D printer", "Electronics");
+			var machine2 = new Machine("Speaker", "Electronics");
+			machineList.add(machine1);
+			machineList.add(machine2);
+
+			Part part = partRepo.save(new Part("Printer nozzle"));
+			Subassembly subassembly = subassemblyRepo.save(new Subassembly("Printer head"));
+
+			Order order = orderRepo.save(new Order());
+
+			order.setOrderDate(LocalDateTime.now());
+			order.setAddress(address);
+			order.setCustomer(customer);
+			order.setMachines(machineList);
+
+			orderRepo.save(order);
+		};
+	}
 }
