cefd6236486b302a6953ad2a1d8343d413d7f87b
NXTDTA <Henry@nextdata.no>
2023-12-07 19:02:03 +0100

    mock test

---
+++ machine-factory/src/test/java/no/pgr209/machinefactory/Order/OrderIntegrationTest.java
@@ -0,0 +1,42 @@
+package no.pgr209.machinefactory.Order;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.web.servlet.MockMvc;
+import org.junit.jupiter.api.Test;
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@SpringBootTest
+@AutoConfigureMockMvc
+public class OrderIntegrationTest {
+
+    @Autowired
+    MockMvc mockMvc;
+
+    @Test
+    void shouldFetchOrders() throws Exception {
+        mockMvc.perform(get("/api/order"))
+                .andExpect(status().isOk())
+                .andDo(result -> {
+                    System.out.println(result.getResponse().getContentAsString());
+                });
+    }
+
+    @Test
+    void shouldFetchOrderById() throws Exception {
+        mockMvc.perform(get("/api/order/1"))
+                .andExpect(status().isOk())
+                .andExpect(MockMvcResultMatchers.jsonPath("$.orderId").value(1))
+                .andExpect(MockMvcResultMatchers.jsonPath("$.customer.customerName").value("Ola Nordmann"))
+                .andExpect(MockMvcResultMatchers.jsonPath("$.address.addressStreet").value("Storgata 33"))
+                .andExpect(MockMvcResultMatchers.jsonPath("$.machines.[0].machineType").value("Electronics"))
+                .andDo(result -> {
+                    System.out.println(result.getResponse().getContentAsString());
+
+                });
+    }
+}
