da8bd183b8d6809d0e764a8c51243d93e2de3f31
Henry M <Henry@nextdata.no>
2023-12-08 14:29:14 +0100

    another mock MVC test order

--- machine-factory/src/test/java/no/pgr209/machinefactory/Order/OrderIntegrationTest.java
+++ machine-factory/src/test/java/no/pgr209/machinefactory/Order/OrderIntegrationTest.java
@@ -1,13 +1,17 @@
 package no.pgr209.machinefactory.Order;
 
+import no.pgr209.machinefactory.service.DataFeedService;
+import org.junit.jupiter.api.BeforeEach;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
 import org.junit.jupiter.api.Test;
 import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
 
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @SpringBootTest
@@ -15,7 +19,15 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 public class OrderIntegrationTest {
 
     @Autowired
+    DataFeedService dataFeedService;
+
+    @Autowired
     MockMvc mockMvc;
+
+    @BeforeEach
+    void setUp() {
+        dataFeedService.initializeData();
+    }
 
     @Test
     void shouldFetchOrders() throws Exception {
@@ -36,7 +48,34 @@ public class OrderIntegrationTest {
                 .andExpect(MockMvcResultMatchers.jsonPath("$.machines.[0].machineType").value("Electronics"))
                 .andDo(result -> {
                     System.out.println(result.getResponse().getContentAsString());
-
                 });
     }
+
+    @Test
+    void shouldCreateOrder() throws Exception {
+        long customerId = 1L;
+        long addressId = 1L;
+        long machineId1 = 1L;
+        long machineId2 = 2L;
+
+        String orderJson = String.format("""
+            {
+                "customerId": %d,
+                "addressId": %d,
+                "machineIds": [%d, %d],
+                "orderDate": "2023-01-01T00:00:00"
+            }
+            """, customerId, addressId, machineId1, machineId2);
+
+        mockMvc.perform(post("/api/order")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(orderJson))
+                .andExpect(status().isOk())
+                .andReturn();
+    }
+
+
+
+
+
 }
