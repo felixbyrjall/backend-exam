f99b28818dcd638d1695d79f858aafdb603e0e0d
NXTDTA <Henry@nextdata.no>
2023-12-09 14:39:07 +0100

    shouldFetchAllOrdersWithPagination can now be used with List again, given recent changes to services. Removed unused imports.

--- machine-factory/src/test/java/no/pgr209/machinefactory/Order/OrderServiceUnitTest.java
+++ machine-factory/src/test/java/no/pgr209/machinefactory/Order/OrderServiceUnitTest.java
@@ -12,7 +12,6 @@ import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageImpl;
 import org.springframework.data.domain.PageRequest;
-import org.springframework.http.ResponseEntity;
 
 import java.util.List;
 import java.util.stream.Collectors;
