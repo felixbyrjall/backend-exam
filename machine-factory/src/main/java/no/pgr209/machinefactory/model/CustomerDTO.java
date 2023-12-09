124179275356081c10edbcbdc311a59fe463b5b1
Felix Byrjall <felixbyrjall@gmail.com>
2023-12-09 17:58:16 +0100

    Updated CustomerDTO

--- machine-factory/src/main/java/no/pgr209/machinefactory/model/CustomerDTO.java
+++ machine-factory/src/main/java/no/pgr209/machinefactory/model/CustomerDTO.java
@@ -3,8 +3,13 @@ package no.pgr209.machinefactory.model;
 import lombok.Getter;
 import lombok.Setter;
 
+import java.util.List;
+
 @Getter
 @Setter
 public class CustomerDTO {
-    private Long addressId;
+    private String customerName;
+    private String customerEmail;
+    private List<Long> addressId;
+    private List<Long> orderId;
 }
