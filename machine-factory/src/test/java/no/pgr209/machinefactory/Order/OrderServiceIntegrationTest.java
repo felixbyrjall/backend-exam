97907fdb411401fcb992cd87cde695c7240544b5
NXTDTA <Henry@nextdata.no>
2023-12-09 17:27:31 +0100

    Adapted shouldFetchOrders / OrderServiceIntegrationTest

--- machine-factory/src/test/java/no/pgr209/machinefactory/Order/OrderServiceIntegrationTest.java
+++ machine-factory/src/test/java/no/pgr209/machinefactory/Order/OrderServiceIntegrationTest.java
@@ -6,7 +6,6 @@ import no.pgr209.machinefactory.service.OrderService;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.data.domain.Pageable;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.transaction.annotation.Transactional;
 
