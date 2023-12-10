b89d0c7208ae44fa2704cb56bc970dab23512a5f
NXTDTA <Henry@nextdata.no>
2023-12-10 16:27:02 +0100

    Redo - OrderServiceUnitTest. Created first mock unit test for service. shouldReturnAllOrders

--- machine-factory/src/test/java/no/pgr209/machinefactory/Order/OrderServiceUnitTest.java
+++ machine-factory/src/test/java/no/pgr209/machinefactory/Order/OrderServiceUnitTest.java
@@ -1,21 +1,17 @@
 package no.pgr209.machinefactory.Order;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.mockito.ArgumentMatchers.any;
 import no.pgr209.machinefactory.model.Order;
 import no.pgr209.machinefactory.repo.AddressRepo;
 import no.pgr209.machinefactory.repo.CustomerRepo;
 import no.pgr209.machinefactory.repo.MachineRepo;
 import no.pgr209.machinefactory.repo.OrderRepo;
 import no.pgr209.machinefactory.service.OrderService;
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.data.domain.Page;
-import org.springframework.data.domain.PageImpl;
-import org.springframework.data.domain.PageRequest;
+
 
 import java.util.ArrayList;
 import java.util.List;
