799f65df373a984c64e1152efe61877f72b20c0d
NXTDTA <Henry@nextdata.no>
2023-12-10 16:26:52 +0100

    Redo - OrderServiceUnitTest. Created first mock unit test for service. shouldReturnAllOrders

--- machine-factory/src/test/java/no/pgr209/machinefactory/Order/OrderServiceUnitTest.java
+++ machine-factory/src/test/java/no/pgr209/machinefactory/Order/OrderServiceUnitTest.java
@@ -3,16 +3,21 @@ package no.pgr209.machinefactory.Order;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.mockito.ArgumentMatchers.any;
 import no.pgr209.machinefactory.model.Order;
+import no.pgr209.machinefactory.repo.AddressRepo;
+import no.pgr209.machinefactory.repo.CustomerRepo;
+import no.pgr209.machinefactory.repo.MachineRepo;
 import no.pgr209.machinefactory.repo.OrderRepo;
 import no.pgr209.machinefactory.service.OrderService;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-import org.springframework.beans.factory.annotation.Autowired;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageImpl;
 import org.springframework.data.domain.PageRequest;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.stream.Collectors;
 import java.util.stream.IntStream;
@@ -22,33 +27,31 @@ import static org.mockito.Mockito.when;
 @SpringBootTest
 public class OrderServiceUnitTest {
 
-    @MockBean
+    @Mock
     private OrderRepo orderRepo;
 
-    @Autowired
+    @Mock
+    private CustomerRepo customerRepo;
+
+    @Mock
+    private AddressRepo addressRepo;
+
+    @Mock
+    private MachineRepo machineRepo;
+
+    @InjectMocks
     private OrderService orderService;
 
-    @Test // Fetch all orders
-    void shouldFetchAllOrders(){
-        List<Order> orderList = List.of(new Order(), new Order());
-        when(orderRepo.findAll()).thenReturn(orderList);
+    @Test
+    void shouldReturnAllOrders() {
 
-        var orders = orderService.getAllOrders();
-        assert orders.size() == 2;
+        List<Order> mockOrders = new ArrayList<>();
+        when(orderRepo.findAll()).thenReturn(mockOrders);
+
+        List<Order> orders = orderService.getAllOrders();
+
+        assertEquals(mockOrders, orders);
     }
 
-    @Test // Fetch all orders for any page, with the pagination method.
-    void shouldFetchAllOrdersWithPagination(){
-        List<Order> allOrders = IntStream.range(0, 12)
-                .mapToObj(i -> new Order())
-                .collect(Collectors.toList());
-
-        Page<Order> orderPage = new PageImpl<>(allOrders);
-        when(orderRepo.findAll(any(PageRequest.class))).thenReturn(orderPage);
-
-        List<Order> orders = orderService.getOrdersByPage(0);
-
-        assertEquals(12, orders.size());
-    }
 
 }
