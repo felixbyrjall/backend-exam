e135280b247b3404eaadd9bc5dad9061df8841b7
NXTDTA <Henry@nextdata.no>
2023-12-10 16:27:14 +0100

    Redo - OrderServiceUnitTest. Created first mock unit test for service. shouldReturnAllOrders

--- machine-factory/src/test/java/no/pgr209/machinefactory/Order/OrderServiceUnitTest.java
+++ machine-factory/src/test/java/no/pgr209/machinefactory/Order/OrderServiceUnitTest.java
@@ -15,8 +15,7 @@ import org.springframework.boot.test.context.SpringBootTest;
 
 import java.util.ArrayList;
 import java.util.List;
-import java.util.stream.Collectors;
-import java.util.stream.IntStream;
+
 
 import static org.mockito.Mockito.when;
 
