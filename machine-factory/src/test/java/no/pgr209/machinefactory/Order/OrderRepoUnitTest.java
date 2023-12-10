6aedba792e870cfdb8af34d53e2f61441d2c27d6
NXTDTA <Henry@nextdata.no>
2023-12-10 12:30:34 +0100

    Renamed unit test - findAll, and changed assert to the used convention.

--- machine-factory/src/test/java/no/pgr209/machinefactory/Order/OrderRepoUnitTest.java
+++ machine-factory/src/test/java/no/pgr209/machinefactory/Order/OrderRepoUnitTest.java
@@ -11,7 +11,6 @@ import java.util.List;
 import java.util.Optional;
 
 import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
-import static org.junit.jupiter.api.Assertions.assertNotNull;
 
 @DataJpaTest
 @ActiveProfiles("dev") // Seperate CommandLine and Data Jpa test.
