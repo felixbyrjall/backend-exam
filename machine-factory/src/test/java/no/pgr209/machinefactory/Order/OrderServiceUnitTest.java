01597ff582be0dbc534518cbd7620a8459440ca5
NXTDTA <Henry@nextdata.no>
2023-12-10 16:27:51 +0100

    Redo - OrderServiceUnitTest. Created first mock unit test for service. shouldReturnAllOrders

--- machine-factory/src/test/java/no/pgr209/machinefactory/Order/OrderServiceUnitTest.java
+++ machine-factory/src/test/java/no/pgr209/machinefactory/Order/OrderServiceUnitTest.java
@@ -2,9 +2,6 @@ package no.pgr209.machinefactory.Order;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import no.pgr209.machinefactory.model.Order;
-import no.pgr209.machinefactory.repo.AddressRepo;
-import no.pgr209.machinefactory.repo.CustomerRepo;
-import no.pgr209.machinefactory.repo.MachineRepo;
 import no.pgr209.machinefactory.repo.OrderRepo;
 import no.pgr209.machinefactory.service.OrderService;
 import org.junit.jupiter.api.Test;
@@ -35,7 +32,7 @@ public class OrderServiceUnitTest {
     @Mock
     private MachineRepo machineRepo;
      */
-    
+
     @InjectMocks
     private OrderService orderService;
 
