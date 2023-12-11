6acf56c4cca1eeb02e5b1aa1533d92eaa543caaf
Felix Byrjall <felixbyrjall@gmail.com>
2023-12-11 14:15:08 +0100

    Updated deletePartById to set attached parts in SubassemblyRepo to null before deleting the Part

--- machine-factory/src/main/java/no/pgr209/machinefactory/service/PartService.java
+++ machine-factory/src/main/java/no/pgr209/machinefactory/service/PartService.java
@@ -2,7 +2,9 @@ package no.pgr209.machinefactory.service;
 
 import no.pgr209.machinefactory.model.Part;
 import no.pgr209.machinefactory.model.PartDTO;
+import no.pgr209.machinefactory.model.Subassembly;
 import no.pgr209.machinefactory.repo.PartRepo;
+import no.pgr209.machinefactory.repo.SubassemblyRepo;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.stereotype.Service;
@@ -12,10 +14,12 @@ import java.util.List;
 @Service
 public class PartService {
     private final PartRepo partRepo;
+    private final SubassemblyRepo subassemblyRepo;
 
     @Autowired
-    public PartService(PartRepo partRepo) {
+    public PartService(PartRepo partRepo, SubassemblyRepo subassemblyRepo) {
         this.partRepo = partRepo;
+        this.subassemblyRepo = subassemblyRepo;
     }
 
     //Get ALL parts
@@ -44,7 +48,18 @@ public class PartService {
     }
 
     public void deletePartById(Long id) {
-        partRepo.deleteById(id);
+        Part part = partRepo.findById(id).orElse(null);
+
+        if(part != null) {
+            for(Subassembly subassembly : subassemblyRepo.findAll()) {
+                if(subassembly.getParts().contains(part)) {
+                    subassembly.getParts().remove(part);
+                    subassemblyRepo.save(subassembly);
+                }
+            }
+
+            partRepo.delete(part);
+        }
     }
 
     public boolean partExists(Long id) {
