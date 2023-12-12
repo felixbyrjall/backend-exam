6839d16be4c5e60ef230c0791338f301b0cbfee6
Felix Byrjall <felixbyrjall@gmail.com>
2023-12-12 21:02:50 +0100

    Created shouldCreateMachine

--- machine-factory/src/test/java/no/pgr209/machinefactory/Subassembly/SubassemblyServiceUnitTest.java
+++ machine-factory/src/test/java/no/pgr209/machinefactory/Subassembly/SubassemblyServiceUnitTest.java
@@ -1,11 +1,9 @@
 package no.pgr209.machinefactory.Subassembly;
 
 import no.pgr209.machinefactory.model.*;
-import no.pgr209.machinefactory.repo.MachineRepo;
 import no.pgr209.machinefactory.repo.PartRepo;
 import no.pgr209.machinefactory.repo.SubassemblyRepo;
 import no.pgr209.machinefactory.service.DataFeedService;
-import no.pgr209.machinefactory.service.MachineService;
 import no.pgr209.machinefactory.service.SubassemblyService;
 import org.junit.jupiter.api.Test;
 import org.mockito.InjectMocks;
@@ -52,5 +50,33 @@ public class SubassemblyServiceUnitTest {
         Subassembly subassembly = subassemblyService.getSubassemblyById(1L);
 
         assertEquals(mockSubassembly, subassembly);
+    }
+
+    @Test // Comprehensive mock & unit-testing, creating a subassembly.
+    void shouldCreateMachine() {
+        SubassemblyDTO subassemblyDTO = new SubassemblyDTO();
+        subassemblyDTO.setSubassemblyName("Voltage Regulator");
+        List<Long> parts = List.of(1L, 2L);
+        subassemblyDTO.setPartId(parts);
+
+        Subassembly mockSubassembly = new Subassembly("Voltage Regulator");
+        Part partOne = new Part( "Capacitor");
+        Part partTwo = new Part( "Resistor");
+
+        when(subassemblyRepo.existsById(1L)).thenReturn(true);
+        when(subassemblyRepo.findById(1L)).thenReturn(Optional.of(mockSubassembly));
+        when(partRepo.existsById(any())).thenReturn(true);
+        when(partRepo.findAllById(parts)).thenReturn(List.of(partOne, partTwo));
+
+        Subassembly createdSubassembly = new Subassembly();
+        createdSubassembly.setSubassemblyName("Voltage Regulator");
+        createdSubassembly.setParts(List.of(partOne, partTwo));
+        when(subassemblyRepo.save(any())).thenReturn(createdSubassembly);
+
+        Subassembly resultSubassembly = subassemblyService.createSubassembly(subassemblyDTO);
+
+        assertThat(resultSubassembly).isNotNull();
+        assertThat(resultSubassembly.getSubassemblyName()).isEqualTo(mockSubassembly.getSubassemblyName());
+        assertThat(resultSubassembly.getParts()).containsExactlyInAnyOrderElementsOf(List.of(partOne, partTwo));
     }
 }
