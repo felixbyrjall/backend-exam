409e68a4b990e5d964097c631ea58ee4fafa1fce
NXTDTA <Henry@nextdata.no>
2023-12-12 19:42:25 +0100

    Created shouldCreateSubassembly

--- machine-factory/src/test/java/no/pgr209/machinefactory/Subassembly/SubassemblyIntegrationTest.java
+++ machine-factory/src/test/java/no/pgr209/machinefactory/Subassembly/SubassemblyIntegrationTest.java
@@ -1,12 +1,15 @@
 package no.pgr209.machinefactory.Subassembly;
 
 import no.pgr209.machinefactory.service.DataFeedService;
+import org.json.JSONObject;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.MvcResult;
 import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -43,6 +46,32 @@ public class SubassemblyIntegrationTest {
                 .andExpect(status().isOk())
                 .andExpect(MockMvcResultMatchers.jsonPath("$.subassemblyId").value(1))
                 .andExpect(MockMvcResultMatchers.jsonPath("$.subassemblyName").value("Printer head"));
+    }
 
+    @Test
+    void shouldCreateSubassembly() throws Exception {
+        String subassemblyJson = """
+        {
+            "subassemblyName": "Printer tags",
+            "partId": []
+        }
+        """;
+
+        // Create the subassembly
+        MvcResult createResult = mockMvc.perform(post("/api/subassembly")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(subassemblyJson))
+                .andExpect(status().isCreated())
+                .andReturn();
+
+        // Extract the subassemblyId from the response
+        String responseContent = createResult.getResponse().getContentAsString();
+        JSONObject jsonObject = new JSONObject(responseContent);
+        int subassemblyId = jsonObject.getInt("subassemblyId");
+
+        mockMvc.perform(get("/api/subassembly/" + subassemblyId))
+                .andExpect(status().isOk())
+                .andExpect(MockMvcResultMatchers.jsonPath("$.subassemblyId").value(subassemblyId))
+                .andExpect(MockMvcResultMatchers.jsonPath("$.subassemblyName").value("Printer tags"));
     }
 }
