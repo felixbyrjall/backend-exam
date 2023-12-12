d1585d4c3598d5cb053f93b16d70620da18de1e7
NXTDTA <Henry@nextdata.no>
2023-12-12 16:44:43 +0100

    Created and tested shouldCreateMachine in MachineIntegrationTest

--- machine-factory/src/test/java/no/pgr209/machinefactory/Machine/MachineIntegrationTest.java
+++ machine-factory/src/test/java/no/pgr209/machinefactory/Machine/MachineIntegrationTest.java
@@ -1,16 +1,20 @@
 package no.pgr209.machinefactory.Machine;
 
 import no.pgr209.machinefactory.service.DataFeedService;
+import org.json.JSONObject;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.MvcResult;
 import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
 import org.springframework.transaction.annotation.Transactional;
 
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @SpringBootTest
@@ -37,5 +41,49 @@ public class MachineIntegrationTest {
                 .andExpect(MockMvcResultMatchers.jsonPath("$[1].machineId").value(2));
     }
 
+    @Test
+    void shouldFetchMachinesOnPage() throws Exception {
+        mockMvc.perform(get("/api/machine/page/0"))
+                .andExpect(status().isOk())
+                .andExpect(MockMvcResultMatchers.jsonPath("$[0].machineId").value(1));
+    }
+
+    @Test
+    void shouldFetchMachineById() throws Exception {
+        mockMvc.perform(get("/api/machine/1"))
+                .andExpect(status().isOk())
+                .andExpect(MockMvcResultMatchers.jsonPath("$.machineId").value(1))
+                .andExpect(MockMvcResultMatchers.jsonPath("$.machineName").value("3D printer"))
+                .andExpect(MockMvcResultMatchers.jsonPath("$.machineType").value("Electronics"));
+    }
+
+    @Test
+    void shouldCreateMachine() throws Exception {
+        String machineJson = """
+        {
+            "machineName": "Robot printer",
+            "machineType":  "Electronics",
+            "subassemblyId": []
+        }
+        """;
+
+        // Create the machine
+        MvcResult createResult = mockMvc.perform(post("/api/machine")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(machineJson))
+                .andExpect(status().isCreated())
+                .andReturn();
+
+        // Extract the machineId from the response
+        String responseContent = createResult.getResponse().getContentAsString();
+        JSONObject jsonObject = new JSONObject(responseContent);
+        int machineId = jsonObject.getInt("machineId");
+
+        mockMvc.perform(get("/api/machine/" + machineId))
+                .andExpect(status().isOk())
+                .andExpect(MockMvcResultMatchers.jsonPath("$.machineId").value(machineId))
+                .andExpect(MockMvcResultMatchers.jsonPath("$.machineName").value("Robot printer"))
+                .andExpect(MockMvcResultMatchers.jsonPath("$.machineType").value("Electronics"));
+    }
 
 }
