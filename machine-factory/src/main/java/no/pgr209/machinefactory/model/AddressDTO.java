f60684a2a3c1c682fdb5046705ceea8af8303733
Felix Byrjall <felixbyrjall@gmail.com>
2023-12-12 13:22:08 +0100

    Added back customers to PUT and POST on address, and added customerId back to AddressDTO

--- machine-factory/src/main/java/no/pgr209/machinefactory/model/AddressDTO.java
+++ machine-factory/src/main/java/no/pgr209/machinefactory/model/AddressDTO.java
@@ -3,10 +3,14 @@ package no.pgr209.machinefactory.model;
 import lombok.Getter;
 import lombok.Setter;
 
+import java.util.List;
+
 @Getter
 @Setter
 public class AddressDTO {
     private String addressStreet;
     private String addressCity;
     private String addressZip;
+
+    private List<Long> customerId;
 }
--- machine-factory/src/main/java/no/pgr209/machinefactory/service/AddressService.java
+++ machine-factory/src/main/java/no/pgr209/machinefactory/service/AddressService.java
@@ -2,20 +2,25 @@ package no.pgr209.machinefactory.service;
 
 import no.pgr209.machinefactory.model.Address;
 import no.pgr209.machinefactory.model.AddressDTO;
+import no.pgr209.machinefactory.model.Customer;
 import no.pgr209.machinefactory.repo.AddressRepo;
+import no.pgr209.machinefactory.repo.CustomerRepo;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.stereotype.Service;
 
+import java.util.Collections;
 import java.util.List;
 
 @Service
 public class AddressService {
     private final AddressRepo addressRepo;
+    private final CustomerRepo customerRepo;
 
     @Autowired
-    public AddressService(AddressRepo addressRepo) {
+    public AddressService(AddressRepo addressRepo, CustomerRepo customerRepo) {
         this.addressRepo = addressRepo;
+        this.customerRepo = customerRepo;
     }
 
     //Get ALL addresses
@@ -50,6 +55,12 @@ public class AddressService {
         }
         newAddress.setAddressZip(addressDTO.getAddressZip());
 
+        List<Long> customerIds = addressDTO.getCustomerId();
+        if(!customerIds.stream().allMatch(customerRepo::existsById)) {
+            return null;
+        }
+        newAddress.setCustomers(customerRepo.findAllById(customerIds));
+
         return addressRepo.save(newAddress);
     }
 
@@ -76,6 +87,17 @@ public class AddressService {
 
             if(addressDTO.getAddressZip() != null) {
                 existingAddress.setAddressZip(addressDTO.getAddressZip());
+            }
+
+            List<Customer> customers = customerRepo.findAllById(addressDTO.getCustomerId());
+
+            if (!addressDTO.getCustomerId().isEmpty()) {
+                if (customers.size() != addressDTO.getCustomerId().size()) {
+                    return null;
+                }
+                existingAddress.setCustomers(customers);
+            } else {
+                existingAddress.setCustomers(Collections.emptyList());
             }
 
             return addressRepo.save(existingAddress);
