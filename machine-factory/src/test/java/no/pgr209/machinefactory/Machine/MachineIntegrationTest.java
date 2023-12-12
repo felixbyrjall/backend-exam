79db1dcc87f423a7a6b9f4d801f55e8e18fead34
NXTDTA <Henry@nextdata.no>
2023-12-12 16:28:23 +0100

    Created and tested shouldFetchMachines in MachineIntegrationTest

--- machine-factory/src/test/java/no/pgr209/machinefactory/Machine/MachineIntegrationTest.java
+++ machine-factory/src/test/java/no/pgr209/machinefactory/Machine/MachineIntegrationTest.java
@@ -7,6 +7,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
 import org.springframework.transaction.annotation.Transactional;
 
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
@@ -31,6 +32,10 @@ public class MachineIntegrationTest {
     @Test
     void shouldFetchMachines() throws Exception {
         mockMvc.perform(get("/api/machine"))
-                .andExpect(status().isOk());
+                .andExpect(status().isOk())
+                .andExpect(MockMvcResultMatchers.jsonPath("$[0].machineId").value(1))
+                .andExpect(MockMvcResultMatchers.jsonPath("$[1].machineId").value(2));
     }
+
+
 }
