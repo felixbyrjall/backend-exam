ee7dc7e02580cbe0272a4115a5e1a1ce26bd85cf
Henry M <Henry@nextdata.no>
2023-12-12 14:45:31 +0100

    Created and tested, shouldReturnAllAddresses in AddressServiceUnitTest class.

---
+++ machine-factory/src/test/java/no/pgr209/machinefactory/Address/AddressServiceUnitTest.java
@@ -0,0 +1,44 @@
+package no.pgr209.machinefactory.Address;
+
+import no.pgr209.machinefactory.model.Address;
+import no.pgr209.machinefactory.model.Customer;
+import no.pgr209.machinefactory.repo.AddressRepo;
+import no.pgr209.machinefactory.repo.CustomerRepo;
+import no.pgr209.machinefactory.service.AddressService;
+import no.pgr209.machinefactory.service.DataFeedService;
+import org.junit.jupiter.api.Test;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.Mockito.when;
+
+@SpringBootTest
+public class AddressServiceUnitTest {
+
+    @Autowired
+    DataFeedService dataFeedService;
+
+    @Mock
+    private CustomerRepo customerRepo;
+
+    @Mock
+    private AddressRepo addressRepo;
+
+    @InjectMocks
+    private AddressService addressService;
+
+    @Test // Mock and test fetching all addresses
+    void shouldReturnAllAddresses() {
+        List<Address> mockAddresses = new ArrayList<>();
+        when(addressRepo.findAll()).thenReturn(mockAddresses);
+        List<Address> addresses = addressService.getAllAddresses();
+
+        assertEquals(mockAddresses, addresses);
+    }
+}
