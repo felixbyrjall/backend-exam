a45630f051101fc190c9fdb7e2d44a06d793242d
NXTDTA <Henry@nextdata.no>
2023-12-14 17:35:51 +0100

    refactor datafeedservice

--- machine-factory/src/main/java/no/pgr209/machinefactory/service/DataFeedService.java
+++ machine-factory/src/main/java/no/pgr209/machinefactory/service/DataFeedService.java
@@ -32,109 +32,71 @@ public class DataFeedService {
 
     @Transactional
     public void initializeData() {
-        // Initialize First Customer and Address
-        Customer customerOne = new Customer("Ola Nordmann", "ola@nordmann.no");
-        Address addressOne = new Address("Storgata 33", "Oslo", "2204");
+        // Initialize Customers and Addresses
+        // Customer One
+        Customer customerOne = customerRepo.save(new Customer("Ola Nordmann", "ola@nordmann.no"));
+        Address addressOne = addressRepo.save(new Address("Storgata 33", "Oslo", "2204"));
         customerOne.getAddresses().add(addressOne);
-        customerOne = customerRepo.save(customerOne);
-        addressOne = addressRepo.save(addressOne);
 
-        // Initialize Second Customer and Address
-        Customer customerTwo = new Customer("Kari Hansen", "kari@hansen.no");
-        Address addressTwo = new Address("Husmannsgate 14", "Oslo", "2232");
-        customerTwo.getAddresses().add(addressOne); // Add same address as first customer.
-        customerTwo.getAddresses().add(addressTwo); // Add a second address
-        customerTwo = customerRepo.save(customerTwo);
-        addressTwo = addressRepo.save(addressTwo);
+        // Customer Two
+        Customer customerTwo = customerRepo.save(new Customer("Kari Hansen", "kari@hansen.no"));
+        Address addressTwo = addressRepo.save(new Address("Husmannsgate 14", "Oslo", "2232"));
+        customerTwo.getAddresses().add(addressOne);
+        customerTwo.getAddresses().add(addressTwo);
 
         // Customer Three
-        Customer customerThree = new Customer("Lars Olsen", "lars@olsen.no");
-        Address addressThree = new Address("Bakkegata 7", "Bergen", "5015");
+        Customer customerThree = customerRepo.save(new Customer("Lars Olsen", "lars@olsen.no"));
+        Address addressThree = addressRepo.save(new Address("Bakkegata 7", "Bergen", "5015"));
         customerThree.getAddresses().add(addressThree);
-        customerThree = customerRepo.save(customerThree);
-        addressThree = addressRepo.save(addressThree);
 
         // Customer Four
-        Customer customerFour = new Customer("Ingrid Solberg", "ingrid@solberg.no");
-        Address addressFour = new Address("Fjellveien 22", "Stavanger", "4021");
+        Customer customerFour = customerRepo.save(new Customer("Ingrid Solberg", "ingrid@solberg.no"));
+        Address addressFour = addressRepo.save(new Address("Fjellveien 22", "Stavanger", "4021"));
         customerFour.getAddresses().add(addressFour);
-        customerFour = customerRepo.save(customerFour);
-        addressFour = addressRepo.save(addressFour);
 
         // Customer Five
-        Customer customerFive = new Customer("Erik Pedersen", "erik@pedersen.no");
-        Address addressFive = new Address("Strandgaten 15", "Trondheim", "7011");
+        Customer customerFive = customerRepo.save(new Customer("Erik Pedersen", "erik@pedersen.no"));
+        Address addressFive = addressRepo.save(new Address("Strandgaten 15", "Trondheim", "7011"));
         customerFive.getAddresses().add(addressFive);
-        customerFive = customerRepo.save(customerFive);
-        addressFive = addressRepo.save(addressFive);
 
         // Customer Six
-        Customer customerSix = new Customer("Mona Johansen", "mona.j@gmail.com");
-        Address addressSix = new Address("Strandveien 10", "Bergen", "5020");
+        Customer customerSix = customerRepo.save(new Customer("Mona Johansen", "mona.j@gmail.com"));
+        Address addressSix = addressRepo.save(new Address("Strandveien 10", "Bergen", "5020"));
         customerSix.getAddresses().add(addressSix);
-        customerSix = customerRepo.save(customerSix);
-        addressSix = addressRepo.save(addressSix);
 
         // Customer Seven
-        Customer customerSeven = new Customer("Andreas Berg", "andreas.berg@yahoo.com");
-        Address addressSeven = new Address("Fossveien 5", "Oslo", "2010");
+        Customer customerSeven = customerRepo.save(new Customer("Andreas Berg", "andreas.berg@yahoo.com"));
+        Address addressSeven = addressRepo.save(new Address("Fossveien 5", "Oslo", "2010"));
         customerSeven.getAddresses().add(addressSeven);
-        customerSeven = customerRepo.save(customerSeven);
-        addressSeven = addressRepo.save(addressSeven);
 
         // Customer Eight
-        Customer customerEight = new Customer("Camilla Larsen", "camilla_l@online.no");
-        Address addressEight = new Address("Bakkegata 15", "Stavanger", "4006");
+        Customer customerEight = customerRepo.save(new Customer("Camilla Larsen", "camilla_l@online.no"));
+        Address addressEight = addressRepo.save(new Address("Bakkegata 15", "Stavanger", "4006"));
         customerEight.getAddresses().add(addressEight);
-        customerEight = customerRepo.save(customerEight);
-        addressEight = addressRepo.save(addressEight);
 
         // Customer Nine
-        Customer customerNine = new Customer("Ole Kristiansen", "ole@kristiansenogco.com");
-        Address addressNine = new Address("Solsiden 8", "Trondheim", "7030");
+        Customer customerNine = customerRepo.save(new Customer("Ole Kristiansen", "ole@kristiansenogco.com"));
+        Address addressNine = addressRepo.save(new Address("Solsiden 8", "Trondheim", "7030"));
         customerNine.getAddresses().add(addressNine);
-        customerNine = customerRepo.save(customerNine);
-        addressNine = addressRepo.save(addressNine);
 
         // Customer Ten
-        Customer customerTen = new Customer("Ingrid Nygaard", "ingrid@nygaard.no");
-        Address addressTen = new Address("Nedre Slottsgate 12", "Oslo", "2015");
+        Customer customerTen = customerRepo.save(new Customer("Ingrid Nygaard", "ingrid@nygaard.no"));
+        Address addressTen = addressRepo.save(new Address("Nedre Slottsgate 12", "Oslo", "2015"));
         customerTen.getAddresses().add(addressTen);
-        customerTen = customerRepo.save(customerTen);
-        addressTen = addressRepo.save(addressTen);
 
         // Initialize Machines
-        Machine machineOne = new Machine("3D printer", "Electronics");
-        machineOne = machineRepo.save(machineOne);
+        Machine machineOne = machineRepo.save(new Machine("3D printer", "Electronics"));
+        Machine machineTwo = machineRepo.save(new Machine("Laser printer", "Electronics"));
+        Machine machineThree = machineRepo.save(new Machine("Circuit Board Assembler", "Assembly"));
+        Machine machineFour = machineRepo.save(new Machine("Soldering Robot", "Assembly"));
+        Machine machineFive = machineRepo.save(new Machine("Microcontroller Programmer", "Electronics"));
+        Machine machineSix = machineRepo.save(new Machine("Surface Mount Technology Machine", "Electronics"));
+        Machine machineSeven = machineRepo.save(new Machine("Pick and Place Machine", "Assembly"));
+        Machine machineEight = machineRepo.save(new Machine("PCB Etching Machine", "Electronics"));
+        Machine machineNine = machineRepo.save(new Machine("Wire Bonding Machine", "Electronics"));
+        Machine machineTen = machineRepo.save(new Machine("Automated Testing Equipment", "Electronics"));
 
-        Machine machineTwo = new Machine("Laser printer", "Electronics");
-        machineTwo = machineRepo.save(machineTwo);
-
-        Machine machineThree = new Machine("Circuit Board Assembler", "Assembly");
-        machineThree = machineRepo.save(machineThree);
-
-        Machine machineFour = new Machine("Soldering Robot", "Assembly");
-        machineFour = machineRepo.save(machineFour);
-
-        Machine machineFive = new Machine("Microcontroller Programmer", "Electronics");
-        machineFive = machineRepo.save(machineFive);
-
-        Machine machineSix = new Machine("Surface Mount Technology Machine", "Electronics");
-        machineSix = machineRepo.save(machineSix);
-
-        Machine machineSeven = new Machine("Pick and Place Machine", "Assembly");
-        machineSeven = machineRepo.save(machineSeven);
-
-        Machine machineEight = new Machine("PCB Etching Machine", "Electronics");
-        machineEight = machineRepo.save(machineEight);
-
-        Machine machineNine = new Machine("Wire Bonding Machine", "Electronics");
-        machineNine = machineRepo.save(machineNine);
-
-        Machine machineTen = new Machine("Automated Testing Equipment", "Electronics");
-        machineTen = machineRepo.save(machineTen);
-
-        // Creating list of machines, to add to orders further down
+        // Gather machines in lists for Orders
         List<Machine> machinesListOne = new ArrayList<>();
         machinesListOne.add(machineOne);
         machinesListOne.add(machineTwo);
@@ -180,37 +142,18 @@ public class DataFeedService {
         machinesListTen.add(machineTen);
 
         // Initialize Subassembly
-        Subassembly subassemblyOne = new Subassembly("Motion Control System");
-        subassemblyOne = subassemblyRepo.save(subassemblyOne);
+        Subassembly subassemblyOne = subassemblyRepo.save(new Subassembly("Motion Control System"));
+        Subassembly subassemblyTwo = subassemblyRepo.save(new Subassembly("Extruder Assembly"));
+        Subassembly subassemblyThree = subassemblyRepo.save(new Subassembly("Robotic Arm System"));
+        Subassembly subassemblyFour = subassemblyRepo.save(new Subassembly("Laser Scanning Unit"));
+        Subassembly subassemblyFive = subassemblyRepo.save(new Subassembly("Toner Cartridge Assembly"));
+        Subassembly subassemblySix = subassemblyRepo.save(new Subassembly("Control System Unit"));
+        Subassembly subassemblySeven = subassemblyRepo.save(new Subassembly("Component Placement Arm"));
+        Subassembly subassemblyEight = subassemblyRepo.save(new Subassembly("Vision Inspection Module"));
+        Subassembly subassemblyNine = subassemblyRepo.save(new Subassembly("Conveyor System"));
+        Subassembly subassemblyTen = subassemblyRepo.save(new Subassembly("Soldering Iron Unit"));
 
-        Subassembly subassemblyTwo = new Subassembly("Extruder Assembly");
-        subassemblyTwo = subassemblyRepo.save(subassemblyTwo);
-
-        Subassembly subassemblyThree = new Subassembly("Robotic Arm System");
-        subassemblyThree = subassemblyRepo.save(subassemblyThree);
-
-        Subassembly subassemblyFour = new Subassembly("Laser Scanning Unit");
-        subassemblyFour = subassemblyRepo.save(subassemblyFour);
-
-        Subassembly subassemblyFive = new Subassembly("Toner Cartridge Assembly");
-        subassemblyFive = subassemblyRepo.save(subassemblyFive);
-
-        Subassembly subassemblySix = new Subassembly("Control System Unit");
-        subassemblySix = subassemblyRepo.save(subassemblySix);
-
-        Subassembly subassemblySeven = new Subassembly("Component Placement Arm");
-        subassemblySeven = subassemblyRepo.save(subassemblySeven);
-
-        Subassembly subassemblyEight = new Subassembly("Vision Inspection Module");
-        subassemblyEight = subassemblyRepo.save(subassemblyEight);
-
-        Subassembly subassemblyNine = new Subassembly("Conveyor System");
-        subassemblyNine = subassemblyRepo.save(subassemblyNine);
-
-        Subassembly subassemblyTen = new Subassembly("Soldering Iron Unit");
-        subassemblyTen = subassemblyRepo.save(subassemblyTen);
-
-        // Add Subassembly to Machine
+        // Add Subassemblies to Machines
         machineOne.getSubassemblies().add(subassemblyOne);
         machineOne.getSubassemblies().add(subassemblyTwo);
 
@@ -249,44 +192,19 @@ public class DataFeedService {
         machineTen.getSubassemblies().add(subassemblyNine);
         machineTen.getSubassemblies().add(subassemblyThree);
 
-        // Save all machines with subassemblies to repo
-        machineRepo.save(machineOne);
-        machineRepo.save(machineTwo);
-        machineRepo.save(machineThree);
-        machineRepo.save(machineFour);
-        machineRepo.save(machineFive);
-        machineRepo.save(machineSix);
-        machineRepo.save(machineSeven);
-        machineRepo.save(machineEight);
-        machineRepo.save(machineNine);
-        machineRepo.save(machineTen);
+        // Initialize Parts
+        Part partOne = partRepo.save(new Part("Fasteners"));
+        Part partTwo = partRepo.save(new Part("Microcontroller"));
+        Part partThree = partRepo.save(new Part("Sensor Module"));
+        Part partFour = partRepo.save(new Part("Power Supply Unit"));
+        Part partFive = partRepo.save(new Part("Display Screen"));
+        Part partSix = partRepo.save(new Part("Communication Module"));
+        Part partSeven = partRepo.save(new Part("Switches"));
+        Part partEight = partRepo.save(new Part("LED indicator"));
+        Part partNine = partRepo.save(new Part("Cables and connectors"));
+        Part partTen = partRepo.save(new Part("Mounting bracket"));
 
-        // Initialize Part and add to Subassembly
-        Part partOne = new Part("Fasteners");
-        Part partTwo = new Part("Microcontroller");
-        Part partThree = new Part("Sensor Module");
-        Part partFour = new Part("Power Supply Unit");
-        Part partFive = new Part("Display Screen");
-        Part partSix = new Part("Communication Module");
-        Part partSeven = new Part("Switches");
-        Part partEight = new Part("LED indicator");
-        Part partNine = new Part("Cables and connectors");
-        Part partTen = new Part("Mounting bracket");
-
-        // Save parts to partRepo
-        partOne = partRepo.save(partOne);
-        partTwo = partRepo.save(partTwo);
-        partThree = partRepo.save(partThree);
-        partFour = partRepo.save(partFour);
-        partFive = partRepo.save(partFive);
-        partSix = partRepo.save(partSix);
-        partSeven = partRepo.save(partSeven);
-        partEight = partRepo.save(partEight);
-        partNine = partRepo.save(partNine);
-        partTen = partRepo.save(partTen);
-
-
-        // Add parts to subassemblies
+        // Add Parts to Subassemblies
         subassemblyOne.getParts().add(partOne);
         subassemblyOne.getParts().add(partFour);
 
@@ -325,89 +243,65 @@ public class DataFeedService {
         subassemblyTen.getParts().add(partFour);
         subassemblyTen.getParts().add(partFive);
 
-        // Update Subassembly with the Part
-        subassemblyRepo.save(subassemblyOne);
-        subassemblyRepo.save(subassemblyTwo);
-        subassemblyRepo.save(subassemblyThree);
-        subassemblyRepo.save(subassemblyFour);
-        subassemblyRepo.save(subassemblyFive);
-        subassemblyRepo.save(subassemblySix);
-        subassemblyRepo.save(subassemblySeven);
-        subassemblyRepo.save(subassemblyEight);
-        subassemblyRepo.save(subassemblyNine);
-        subassemblyRepo.save(subassemblyTen);
-
-        // Initialize and save orders with the appropriate address, customer and list of machines
+        // Initialize Orders with necessary information: addresses, customers and machines
         // Order One
-        Order orderOne = new Order(LocalDateTime.parse("2023-01-19T08:45:21.123456"));
+        Order orderOne = orderRepo.save(new Order(LocalDateTime.parse("2023-01-19T08:45:21.00000")));
         orderOne.setAddress(addressOne);
         orderOne.setCustomer(customerOne);
         orderOne.getMachines().addAll(machinesListOne);
 
         // Order two
-        Order orderTwo = new Order(LocalDateTime.parse("2023-03-07T14:22:59.987654"));
+        Order orderTwo = orderRepo.save(new Order(LocalDateTime.parse("2023-03-07T14:22:59.00000")));
         orderTwo.setAddress(addressEight);
         orderTwo.setCustomer(customerTwo);
         orderTwo.getMachines().addAll(machinesListTwo);
 
         // Order Three
-        Order orderThree = new Order(LocalDateTime.parse("2023-05-15T18:37:44.567890"));
+        Order orderThree = orderRepo.save(new Order(LocalDateTime.parse("2023-05-15T18:37:44.00000")));
         orderThree.setAddress(addressThree);
         orderThree.setCustomer(customerThree);
         orderThree.getMachines().addAll(machinesListThree);
 
         // Order Four
-        Order orderFour = new Order(LocalDateTime.parse("2023-07-02T22:11:33.456789"));
+        Order orderFour = orderRepo.save(new Order(LocalDateTime.parse("2023-07-02T22:11:33.00000")));
         orderFour.setAddress(addressFour);
         orderFour.setCustomer(customerFour);
         orderFour.getMachines().addAll(machinesListFour);
 
         // Order Five
-        Order orderFive = new Order(LocalDateTime.parse("2023-09-11T11:55:27.345678"));
+        Order orderFive = orderRepo.save(new Order(LocalDateTime.parse("2023-09-11T11:55:27.00000")));
         orderFive.setAddress(addressFive);
         orderFive.setCustomer(customerFive);
         orderFive.getMachines().addAll(machinesListFive);
 
         // Order Six
-        Order orderSix = new Order(LocalDateTime.parse("2023-11-23T05:30:15.678901"));
+        Order orderSix = orderRepo.save(new Order(LocalDateTime.parse("2023-11-23T05:30:15.00000")));
         orderSix.setAddress(addressNine);
         orderSix.setCustomer(customerSix);
         orderSix.getMachines().addAll(machinesListSix);
 
         // Order Seven
-        Order orderSeven = new Order(LocalDateTime.parse("2023-12-31T19:48:02.234567"));
+        Order orderSeven = orderRepo.save(new Order(LocalDateTime.parse("2023-12-31T19:48:02.00000")));
         orderSeven.setAddress(addressSeven);
         orderSeven.setCustomer(customerSeven);
         orderSeven.getMachines().addAll(machinesListSeven);
 
         // Order Eight
-        Order orderEight = new Order(LocalDateTime.parse("2023-06-04T03:14:58.876543"));
+        Order orderEight = orderRepo.save(new Order(LocalDateTime.parse("2023-06-04T03:14:58.00000")));
         orderEight.setAddress(addressEight);
         orderEight.setCustomer(customerEight);
         orderEight.getMachines().addAll(machinesListEight);
 
         // Order Nine
-        Order orderNine = new Order(LocalDateTime.parse("2023-08-28T21:09:37.345678"));
+        Order orderNine = orderRepo.save(new Order(LocalDateTime.parse("2023-08-28T21:09:37.00000")));
         orderNine.setAddress(addressTen);
         orderNine.setCustomer(customerNine);
         orderNine.getMachines().addAll(machinesListNine);
 
         // Order Ten
-        Order orderTen = new Order(LocalDateTime.parse("2023-02-10T09:26:47.901234"));
+        Order orderTen = orderRepo.save(new Order(LocalDateTime.parse("2023-02-10T09:26:47.00000")));
         orderTen.setAddress(addressTen);
         orderTen.setCustomer(customerTen);
         orderTen.getMachines().addAll(machinesListTen);
-
-        // Save all orders
-        orderRepo.save(orderOne);
-        orderRepo.save(orderTwo);
-        orderRepo.save(orderThree);
-        orderRepo.save(orderFour);
-        orderRepo.save(orderFive);
-        orderRepo.save(orderSix);
-        orderRepo.save(orderSeven);
-        orderRepo.save(orderEight);
-        orderRepo.save(orderNine);
-        orderRepo.save(orderTen);
     }
 }
