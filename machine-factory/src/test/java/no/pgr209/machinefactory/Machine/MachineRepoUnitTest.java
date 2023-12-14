70197b1accf2f653a3cb3815e62e9b0f23e9fc4c
NXTDTA <Henry@nextdata.no>
2023-12-14 18:13:19 +0100

    Further changes to MachineRepoUnitTest. Test all good.

--- machine-factory/src/test/java/no/pgr209/machinefactory/Machine/MachineRepoUnitTest.java
+++ machine-factory/src/test/java/no/pgr209/machinefactory/Machine/MachineRepoUnitTest.java
@@ -8,7 +8,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.test.context.ActiveProfiles;
 
-import java.util.Arrays;
 import java.util.List;
 import java.util.Optional;
 
