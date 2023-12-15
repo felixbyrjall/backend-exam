8357b9b6347e4158f6b5a5101302a1234ea5004e
NXTDTA <Henry@nextdata.no>
2023-12-15 13:17:46 +0100

    Removed unused service import

--- machine-factory/src/test/java/no/pgr209/machinefactory/Subassembly/SubassemblyServiceIntegrationTest.java
+++ machine-factory/src/test/java/no/pgr209/machinefactory/Subassembly/SubassemblyServiceIntegrationTest.java
@@ -24,9 +24,6 @@ public class SubassemblyServiceIntegrationTest {
     SubassemblyService subassemblyService;
 
     @Autowired
-    PartService partService;
-
-    @Autowired
     MachineRepo machineRepo;
 
     @Autowired
